09201374a82b08c27045e8712bbf990ce3f1a940
Profudeinfo33 <dinu_petre26@yahoo.ro>
2021-11-03 23:33:48 +0200

    create-joke-service

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5jokesappv2.services;
+
+public interface JokesService {
+
+    String getJoke();
+
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
